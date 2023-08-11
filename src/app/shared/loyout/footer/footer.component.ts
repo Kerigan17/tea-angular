a8ef8853fbc209e574ace0c2744b1c9393f22154
Aliaksandr Malko <104769289+Kerigan17@users.noreply.github.com>
2023-08-11 12:54:46 +0300

    rebase height order

--- src/app/shared/loyout/footer/footer.component.ts
+++ src/app/shared/loyout/footer/footer.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'footer-component',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent {
 
--- src/app/shared/loyout/header/header.component.ts
+++ src/app/shared/loyout/header/header.component.ts
@@ -3,8 +3,7 @@ import {Component, OnInit} from '@angular/core';
 declare let $: any;
 @Component({
   selector: 'header-component',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit{
   ngOnInit() {
